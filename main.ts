007c3d92b88ac171c06da12fd4998ed3970d24a7
迷渡 <justjavac@gmail.com>
2022-07-28 17:45:56 +0800

    fix: set baseUrl to github

--- main.ts
+++ main.ts
@@ -29,7 +29,9 @@ async function handleRequest(request: Request) {
 
   const readme = await Deno.readTextFile("./README.md");
 
-  const body = render(readme, { baseUrl: import.meta.url });
+  const body = render(readme, {
+    baseUrl: "https://github.com/justjavac/deno_deploy_count/blob/main",
+  });
   const html = `<!DOCTYPE html>
     <html lang="en">
       <head>
